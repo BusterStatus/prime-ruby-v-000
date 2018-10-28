c67d57a4576dfbeaa470e0a875044fb66abb70ba
matthew.serre <matthew.serre@gmail.com>
2018-10-28 23:59:30 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,14 +1,16 @@
 # Add  code here!
 def prime?(integer)
+  verdict = nil
   if integer < 0
     integer = integer * -1
   end
   array = (2..Math.sqrt(integer)).to_a
   array.each{|x|
     if integer % x == 0
-      FALSE
+      verdict = FALSE
     else
-      TRUE
+      verdict = TRUE
+      break
     end
   }
 end
