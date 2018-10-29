b465c3bc14f1088d0573ccd391f20fdd74e97e8d
matthew.serre <matthew.serre@gmail.com>
2018-10-29 00:26:15 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -4,9 +4,9 @@ def prime?(integer)
   return false if integer < 2
   (2..integer - 1).each do |x|
     if (integer % x) == 0 || integer.even? == true
-      verdict = false
-    else
       verdict = true
+    else
+      verdict = false
     end
   end
   return verdict
