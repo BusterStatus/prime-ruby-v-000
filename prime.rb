960e3851b14b5128c331a603caf48aedcedff6d3
matthew.serre <matthew.serre@gmail.com>
2018-10-29 00:35:42 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -3,7 +3,7 @@ def prime?(integer)
   if integer < 0
     (-2..integer + 1).each {|x| return false if (integer % x) == 0 }
     true
-  elsif integer == 0 || 1
+  elsif integer == 0 || integer == 1
     false
   else
     (2..integer - 1).each {|x| return false if (integer % x) == 0 }
